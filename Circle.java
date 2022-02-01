9886bd90afad330f9d5f48d57a4185bf2e001fc9
NoahCarboMarist <79176945+NoahCarboMarist@users.noreply.github.com>
2022-02-01 17:44:07 -0500

    Add files via upload
    lab3

---
+++ Circle.java
@@ -0,0 +1,9 @@
+package lab3;
+
+public class Circle extends Ellipse {
+	protected int r;
+	
+	public Circle(int r) {
+		super(r,r);
+}
+}
---
+++ Ellipse.java
@@ -0,0 +1,32 @@
+package lab3;
+
+public class Ellipse {
+
+    protected int a;
+    protected int b;
+    
+    public Ellipse(int a, int b) {
+    	this.a = a;
+    	this.b = b;
+    }
+
+	public double area() {
+        
+        return a * b * Math.PI;
+    }
+    
+    public int getWidth() {
+
+    	return a;
+    }
+    
+    public int getHeight() {
+    	return b;
+    }
+    
+    @Override
+    public String toString() {
+      return getClass().getSimpleName() + "(a="+a+", b="+b+")";
+    }
+
+}
